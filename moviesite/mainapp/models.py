63d730cb3d877be98feb25e404cfbda2279c734b
Вячеслав Бородин <vikc003@mail.ru>
2022-04-03 10:53:40 +0500

    create db and make first migrate

--- moviesite/mainapp/models.py
+++ moviesite/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+
+    title = models.CharField(max_length=255, verbose_name='Название фильма')
+    movie_image = models.ImageField(verbose_name='Обложка фильма')
+    rating = models.FloatField(verbose_name='Рейтинг фильма')
+    genre = models.CharField(max_length=255, verbose_name='Жанр')
+    description = models.TextField(verbose_name='Описание фильма')
--- moviesite/moviesite/settings.py
+++ moviesite/moviesite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-67%#0zpy%ra)g-&ue_m@0^tttx0bpuq-e(eb9ca5hab!8v@p!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
