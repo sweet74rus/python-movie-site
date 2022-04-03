877579d382f6230ea16020f754f4a59d3c89d7e1
Вячеслав Бородин <vikc003@mail.ru>
2022-04-03 11:28:36 +0500

    add data

--- moviesite/mainapp/models.py
+++ moviesite/mainapp/models.py
@@ -7,3 +7,6 @@ class Movie(models.Model):
     rating = models.FloatField(verbose_name='Рейтинг фильма')
     genre = models.CharField(max_length=255, verbose_name='Жанр')
     description = models.TextField(verbose_name='Описание фильма')
+
+    def __str__(self):
+        return self.title
