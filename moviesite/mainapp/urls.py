638e1cdb072c9badf14420070ab2300cdb224bc6
Вячеслав Бородин <vikc003@mail.ru>
2022-04-04 15:28:00 +0500

    add templates

--- moviesite/mainapp/urls.py
+++ moviesite/mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from mainapp.views import *
 
 urlpatterns = [
-    path('', main_page),
-    path('categories/<slug:category_name>/', categories)
+    path('', main_page, name='home'),
+    path('categories/<slug:category_name>/', categories),
+    path('about/', about, name='about')
 ]
--- moviesite/mainapp/views.py
+++ moviesite/mainapp/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from .models import *
+
+movies = Movie.objects.all()
 
 def main_page(request):
-    return HttpResponse('Страница киносайта')
+    return render(request, 'mainapp/index.html', {'title': 'Главная', 'movies': movies})
+
+def about(request):
+    return render(request, 'mainapp/about.html', {'title': 'О сайте'})
 
 def categories(request, category_name):
     return HttpResponse(f"<h1>Категории фильмов</h1><p>{category_name}</p>")
