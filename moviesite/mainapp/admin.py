e392c8d62ce794388ecbb41efd2299cba9ae5e43
Вячеслав Бородин <vikc003@mail.ru>
2022-04-16 12:09:05 +0500

    setting admin-panel

--- moviesite/mainapp/admin.py
+++ moviesite/mainapp/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'rating', 'genre')
+    list_display = ('id', 'title', 'rating', 'get_html_image', 'genre')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'genre')
     prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'genre', 'rating', 'movie_image', 'get_html_image', 'description')
+    readonly_fields = ('get_html_image',)
+    save_on_top = True
 
+    def get_html_image(self, object):
+        return mark_safe(f"<img src='{object.movie_image.url}' width=80>")
+
+    get_html_image.short_description = 'Обложка'
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
@@ -15,3 +24,6 @@ class GenreAdmin(admin.ModelAdmin):
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Genre, GenreAdmin)
+
+admin.site.site_title = 'Админ-панель киносайта'
+admin.site.site_header = 'Админ-панель киносайта'
